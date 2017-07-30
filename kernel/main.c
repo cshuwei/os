896d87afe31b4493c1f6c791b573fc85cd0ba015
cshuwei <806695981@qq.com>
2017-07-30 15:15:38 +0800

    thread

--- kernel/main.c
+++ kernel/main.c
@@ -1,8 +1,34 @@
 #include "print.h"
 #include "init.h"
+#include "thread.h"
+
+void k_thread_hello(void*);
+void k_thread_bye(void*);
 void main(void) {
    put_str("I am kernel\n");
    init_all();
-   asm volatile（"sti"）;
-   while(1);
+
+   thread_start("k_thread_hello", 31, k_thread_hello, "hello_world");
+   thread_start("k_thread_bye", 31, k_thread_bye, "bye_world");
+   int_enable();
+   while(1)
+   	{
+   		put_str("I am Main thread");
+   	}
+}
+
+void k_thread_hello(void* arg) {     
+
+   char* para = arg;
+   while(1) {
+      put_str(para);
+   }
+}
+
+void k_thread_bye(void* arg) {     
+
+   char* para = arg;
+   while(1) {
+      put_str(para);
+   }
 }
