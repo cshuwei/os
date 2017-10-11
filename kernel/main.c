5719b89f1aa7d43dc34e71e6b769349bca8b2b63
cshuwei <806695981@qq.com>
2017-10-11 13:38:59 +0800

    put_str

--- kernel/main.c
+++ kernel/main.c
@@ -1,60 +1,11 @@
 #include "print.h"
-#include "init.h"
-#include "thread.h"
-#include "interrupt.h"
-#include "console.h"
-#include "process.h"
-#include "syscall-init.h"
-#include "syscall.h"
-#include "stdio.h"
-#include "memory.h"
-#include "dir.h"
-#include "fs.h"
-#include "assert.h"
-#include "shell.h"
-
-#include "ide.h"
-#include "stdio-kernel.h"
-
-void init(void);
-
-int main(void) {
-   put_str("I am kernel\n");
-   init_all();
-
-/*************    write application start   *************/
-//   uint32_t file_size = 5698; 
-//   uint32_t sec_cnt = DIV_ROUND_UP(file_size, 512);
-//   struct disk* sda = &channels[0].devices[0];
-//   void* prog_buf = sys_malloc(file_size);
-//   ide_read(sda, 300, prog_buf, sec_cnt);
-//   int32_t fd = sys_open("/cat", O_CREAT|O_RDWR);
-//   if (fd != -1) {
-//      if(sys_write(fd, prog_buf, file_size) == -1) {
-//         printk("file write error!\n");
-//         while(1);
-//      }
-//   }
-/*************    write application end  *************/
-   cls_screen();
-   console_put_str("[rabbit@localhost /]$ ");
-   thread_exit(running_thread(), true);
-   return 0;
+void main(void){
+	put_str("Hello, I'm kernel.\n");
+	put_int(0);
+	put_char('\n');
+	put_int(0x1234567a9);
+	put_char('\n');
+	put_int(0x00000000);
+	while(1);
 }
 
-
-void init(void) {
-   uint32_t ret_pid = fork();
-   if(ret_pid) {  
-      int status;
-      int child_pid;
-     
-       while(1) {
-     child_pid = wait(&status);
-     printf("I`m init, My pid is 1, I recieve a child, It`s pid is %d, status is %d\n", child_pid, status);
-       }
-   } else {   
-      my_shell();
-   }
-   panic("init: should not be here");
-}
--- lib/kernel/print.h
+++ lib/kernel/print.h
@@ -1,8 +1,7 @@
-#ifndef __LIB_KERNEL_PRINT_H
+#ifdef 	__LIB_KERNEL_PRINT_H
 #define __LIB_KERNEL_PRINT_H
 #include "stdint.h"
 void put_char(uint8_t char_asci);
-void put_str(char* string);
-void put_int(uint32_t num);	
+void put_str(char* message);
+void put_int(uint32_t num);
 #endif
-
