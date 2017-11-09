41d75cbfe966999b9f1954518e499ec936fb7421
cshuwei <806695981@qq.com>
2017-11-09 14:44:02 +0800

    add printf

--- kernel/main.c
+++ kernel/main.c
@@ -7,6 +7,7 @@
 #include "process.h"
 #include "syscall-init.h"
 #include "syscall.h"
+#include "stdio.h"
 void k_thread_a(void* arg);
 void k_thread_b(void* arg);
 void u_prog_a(void);
@@ -39,9 +40,6 @@ void k_thread_a(void* arg) {
     console_put_str(" thread_a_pid:0x");
     console_put_int(sys_getpid());
     console_put_char('\n');
-    console_put_str(" prog_a_pid:0x");
-    console_put_int(prog_a_pid);
-    console_put_char('\n');
 	while(1); 
 }
 
@@ -50,18 +48,17 @@ void k_thread_b(void* arg) {
     console_put_str(" thread_b_pid:0x");
     console_put_int(sys_getpid());
     console_put_char('\n');
-    console_put_str(" prog_b_pid:0x");
-    console_put_int(prog_b_pid);
-    console_put_char('\n');
 	while(1); 
 }
 void u_prog_a(void) {
-    prog_a_pid = getpid();
+    char* name = "prog_a";
+    printf("I am %s, my a_pid:%d%c", name, getpid(), '\n');
     while(1);       
 }
 
 void u_prog_b(void) {
-    prog_b_pid = getpid();
+    char* name = "prog_b";
+    printf("I am %s, my b_pid:%d%c", name, getpid(), '\n');
     while(1);
 }
 
--- lib/stdio.h
+++ lib/stdio.h
@@ -2,7 +2,8 @@
 #define __LIB_STDIO_H
 #include "stdint.h"
 typedef char* va_list;
-uint32_t printf(const char* str, ...);
-uint32_t vsprintf(char* str, const char* format, va_list ap);
+uint32_t printf(const char* formate, ...);
 uint32_t sprintf(char* buf, const char* format, ...);
+uint32_t vsprintf(char* str, const char* format, va_list ap);
 #endif
+
