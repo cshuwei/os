5c495a9645f689c98cea9d3702c737c29163318b
cshuwei <806695981@qq.com>
2017-12-03 19:21:29 +0800

    add sys_opendir and sys_closedir

--- fs/fs.c
+++ fs/fs.c
@@ -526,3 +526,37 @@ rollback:
     sys_free(io_buf);
     return -1;
 }
+
+
+struct dir* sys_opendir(const char* name) {
+    ASSERT(strlen(name) < MAX_PATH_LEN);
+    if (name[0] == '/' && (name[1] == 0 || name[0] == '.')) {
+        return &root_dir;
+    }
+    struct path_search_record searched_record;
+    memset(&searched_record, 0, sizeof(struct path_search_record));
+    int inode_no = search_file(name, &searched_record);
+    struct dir* ret = NULL;
+    if (inode_no == -1) {
+        printk("In %s, sub path %s not exist\n", name, searched_record.searched_path);
+    } else {
+        if (searched_record.file_type == FT_REGULAR) {
+           printk("%s is regular file!\n", name); 
+        } else if (searched_record.file_type == FT_DIRECTORY) {
+            ret = dir_open(cur_part, inode_no);
+        }
+    }
+    dir_close(searched_record.parent_dir);
+    return ret;
+}
+
+int32_t sys_closedir(struct dir* dir) {
+    int32_t ret = -1;
+    if (dir != NULL) {
+        dir_close(dir);
+        ret = 0;
+    }
+    return ret;
+}
+
+
--- kernel/main.c
+++ kernel/main.c
@@ -72,6 +72,18 @@ int main(void){
         printf("/dir1/subdir1/file2 says:\n%s", buf);
         sys_close(fd);
     }
+
+    struct dir* p_dir = sys_opendir("/dir1/subdir1");
+    if (p_dir) {
+        printf("/dir1/subdir1 open done!\n");
+        if (sys_closedir(p_dir) == 0) {
+            printf("/dir1/subdir1 close done!\n");
+        } else {
+            printf("/dir1/subdir1 close fail!\n");
+        }
+    } else {
+        printf("/dir1/subdir1 open fail!\n");
+    }
     while(1);//{ 
 //        	console_put_str("MAIN ");
 //	}
